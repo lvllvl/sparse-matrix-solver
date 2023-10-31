65b23aa51834d922c7f9e963741b794b61c0fd36
lvllvl <chrisidxloop@gmail.com>
2023-10-31 18:29:53 -0400

    backend: found a way to make it work

--- rust-backend/src/main.rs
+++ rust-backend/src/main.rs
@@ -1,57 +1,57 @@
-#![feature(decl_macro)]
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+extern crate rocket_cors;
 
 use rand::Rng;
-use rocket::{get, routes};
-use rocket::serde::json::Json;
-use serde::Serialize;
-use rocket_cors::{AllowedOrigins, Cors, CorsOptions};
+use rocket::http::Method;
+use rocket_contrib::json::Json;
+use rocket_cors::{AllowedHeaders, AllowedOrigins, CorsOptions};
+use serde_derive::Serialize;
 
-
-type Matrix = Vec< Vec< f64 > > ;
+type Matrix = Vec<Vec<f64>>;
 
 #[derive(Serialize)]
-struct Response { 
+struct Response {
     matrix: Matrix,
 }
 
-
 fn main() {
-    let rocket_instance = rocket::build().mount("/", routes![api_generate_sparse_matrix]);
-    rocket_instance.launch();
-
     let allowed_origins = AllowedOrigins::some_exact(&["http://localhost:3000"]);
 
-    let cors_1 = CorsOptions {
+    let cors = CorsOptions {
         allowed_origins,
+        allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(),
+        allowed_headers: AllowedHeaders::some(&["Authorization", "Accept"]),
+        allow_credentials: true,
         ..Default::default()
     }
     .to_cors()
-    .expect("To configure CORS");
+    .unwrap();
 
-    rocket::build()
-        .attach( cors_1 )
-        .mount( "/", routes![ api_generate_sparse_matrix ])
+    rocket::ignite()
+        .mount("/", routes![api_generate_sparse_matrix])
+        .attach(cors)
         .launch();
 }
 
+fn generate_sparse_matrix(m: usize, n: usize, s: f64) -> Matrix {
+    let mut rng = rand::thread_rng();
+    let mut matrix = vec![vec![0.0; n]; m];
 
-fn generate_sparse_matrix( m: usize, n: usize, s: f64 ) -> Matrix {
-    let mut rng = rand::thread_rng(); 
-    let mut matrix = vec![ vec![ 0.0; n ]; m ]; 
+    let total_elements = m * n;
+    let non_zero_elements = (s * total_elements as f64).ceil() as usize;
 
-    let total_elements = m * n ;
-    let non_zero_elements = ( s * total_elements as f64 ).ceil() as usize;
+    for _ in 0..non_zero_elements {
+        let mut row = rng.gen_range(0..m);
+        let mut col = rng.gen_range(0..n);
 
-    for _ in 0..non_zero_elements { 
-        let mut row = rng.gen_range( 0..m ); 
-        let mut col = rng.gen_range( 0..n );
-
-        while matrix[ row ][ col ] != 0.0 {
-            row = rng.gen_range( 0..m ); 
-            col = rng.gen_range( 0..n );
+        while matrix[row][col] != 0.0 {
+            row = rng.gen_range(0..m);
+            col = rng.gen_range(0..n);
         }
 
-        matrix[ row ][ col ] = rng.gen::< f64 >();
+        matrix[row][col] = rng.gen::<f64>();
     }
     matrix
 }
