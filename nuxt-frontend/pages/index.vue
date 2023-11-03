ba033a8ed21a06a68360390b0d4fd7c11a5fe972
lvllvl <chrisidxloop@gmail.com>
2023-11-02 22:36:23 -0400

    geez ok this works now

--- nuxt-frontend/pages/index.vue
+++ nuxt-frontend/pages/index.vue
@@ -1,34 +1,35 @@
 <template>
-
-  <div class="root"> <!-- Main container -->
+  <div class="root">
+    <!-- Main container -->
     <header>
       <h1>Sparse Matrix Solver</h1>
     </header>
-  
+
     <!-- Matrix and Divider Section -->
     <div class="container">
-
-       <!-- Left Side -->
-    <div class="matrix-section">
-      <div class="matrix-box red-border">
-        <!-- Displaying the generated sparse matrix using MathJax -->
-        <div v-html="matrixLatex"></div>
-        <div v-if="error">
-          Error: {{ error }}
+      <!-- Left Side -->
+      <div class="matrix-section">
+        <!-- Content of the left matrix box -->
+        <div class="matrix-box red-border">
+          <!-- Displaying the generated sparse matrix using MathJax -->
+          <div v-html="matrixLatex"></div>
+          <div v-if="error">Error: {{ error }}</div>
         </div>
       </div>
-    </div>
-
-
 
       <div class="divider"></div>
 
       <!-- Right Side -->
       <div class="matrix-section">
-        <div class="matrix-box blue-border"></div>
+        <!-- Content of the right matrix box -->
+        <div class="matrix-box blue-border">
+          <!-- Displaying the solution matrix using MathJax-->
+          <div v-html="solutionLatex"></div>
+          <div v-if="error">Error: {{ error }}</div>
+        </div>
       </div>
     </div>
-    
+
     <!-- Buttons Section -->
     <div class="buttons-container">
       <button @click="generateMatrix">Generate Sparse Matrix</button>
@@ -39,91 +40,113 @@
 
 
 
+
 <script>
 export default {
   data() {
     return {
       sparseMatrix: [], // This will store the generated sparse matrix
-      matrixLatex: '', // This will store the LaTex representation of the matrix 
+      matrixLatex: "", // This will store the LaTex representation of the matrix
       error: null, // To handle any errors during the API request
-      solution: null // store the solution 
+      solution: null, // store the solution
+      solutionLatex: "", // store the LaTeX representation of the solution
     };
   },
   computed: {
     formattedMatrixLatex() {
-          let latex = '\\begin{bmatrix}'; // Start of matrix in LaTeX
-          for ( let row of this.sparseMatrix ) {
-            latex += row.join(' & ') + ' \\\\'; 
-          }
-          latex += '\\end{bmatrix}'; // End of matrix in LaTeX
-          return `$${latex}$`; // Wrap with `$` delimiters
-          }
+      let latex = "\\begin{bmatrix}"; // Start of matrix in LaTeX
+      for (let row of this.sparseMatrix) {
+        latex += row.join(" & ") + " \\\\";
+      }
+      latex += "\\end{bmatrix}"; // End of matrix in LaTeX
+      return `$${latex}$`; // Wrap with `$` delimiters
     },
+  },
   methods: {
     async generateMatrix() {
+      this.error = null; // Clear previous errors, if any
+      this.sparseMatrix = []; // Clear the previous matrix, if any
       try {
-        const response = await fetch('http://localhost:8000/generateMatrix', { method: 'POST' });
-        console.log( "Raw Response: ", response ); 
+        const response = await fetch("http://localhost:8000/generateMatrix", {
+          method: "POST",
+          headers: {
+            "Cache-Control": "no-cache", // Add this line to prevent caching
+          },
+        });
+        console.log("Raw Response: ", response);
 
-        if (!response.ok) {
-          const text = await response.text(); // Get response as text
-          console.log( "Server Response: ", text ); // Log it ! 
+        if (response.ok) {
+          const data = await response.json();
+          this.sparseMatrix = data.matrix;
+          this.matrixLatex = this.formattedMatrixLatex;
+          console.log(this.matrixLatex);
+          if (
+            window.MathJax &&
+            typeof window.MathJax.typesetPromise === "function"
+          ) {
+            await window.MathJax.typesetPromise();
+          }
+        } else {
+          const text = await response.text();
+          console.log("Server Response: ", text);
           throw new Error(`Network response was not ok: ${text}`);
         }
-
-        if ( response.bodyUsed ) {
-          const data = await response.json(); // Assuming the backend returns the matrix as a JSON
-          this.sparseMatrix = data.matrix;
-          this.matrixLatex = this.formattedMatrixLatex; // Update the LaTex representation of the matrix
-          console.log( this.matrixLatex ); // Log the LaTex representation of the matrix
-
-          // Update MathJax rendering 
-          if ( window.MathJax && typeof window.MathJax.typesetPromise === 'function' ){
-            await window.MathJax.typesetPromise(); // Force mathJax to update
-          }
-
-        } else {
-          console.error( "Response body is empty" ); 
-        }
-
       } catch (error) {
-        console.error( "Failed to generate matrix: ", error.message );
+        console.error("Failed to generate matrix: ", error.message);
         this.error = "Failed to generate matrix: " + error.message;
       }
     },
     async solveMatrix() {
       try {
         // First, solve the matrix
-        await fetch( 'http://localhost:8000/solveMatrix', { method: 'POST'} );
+        await fetch("http://localhost:8000/solveMatrix", { method: "POST" });
 
-        // THen, get the solution
-        const response = await fetch( 'http://localhost:8000/getSolution' );
-        if ( !response.ok ) {
-          throw new Error( 'Failed to retrieve solution' );
+        // Then, get the solution
+        const response = await fetch("http://localhost:8000/getSolution");
+        if (!response.ok) {
+          throw new Error("Failed to retrieve solution");
         }
+
         const data = await response.json();
         this.solution = data.solution;
-        console.log( "Solution: ", this.solution );
-        // TODO: display solution in the UI
+        console.log("Solution: ", this.solution);
+
+        // Here you would update the UI to display the solution
+        // For example, if you had a 'solutionLatex' data property, you could format the solution as LaTeX
+        this.solutionLatex = this.formatSolutionToLatex(this.solution);
+
+        // Update MathJax rendering
+        if (
+          window.MathJax &&
+          typeof window.MathJax.typesetPromise === "function"
+        ) {
+          await window.MathJax.typesetPromise(); // Force MathJax to update
+        }
       } catch (error) {
-        console.error( "failed to solve matrix: ", error.message)
+        console.error("Failed to solve matrix: ", error.message);
       }
-    }
-   },
+    },
+
+    // Add this new method to format the solution as LaTeX
+    formatSolutionToLatex(solution) {
+      if (!solution) return "";
+      let latex = solution.map((value) => value.toFixed(2)).join(" & ");
+      return `\\begin{bmatrix}${latex}\\end{bmatrix}`;
+    },
+  },
   head() {
     return {
-      title: 'Sparse Matrix Solver'
+      title: "Sparse Matrix Solver",
     };
-  }
+  },
 };
 </script>
-
 
 <style scoped>
 button {
   padding: 10px 20px;
   font-size: 16px;
-  margin: 0 5px; 
+  margin: 0 5px;
 }
 
 header {
@@ -140,21 +163,25 @@ table {
   width: 100%;
   max-height: 38vw;
   border-collapse: collapse;
-  overflow: hidden; /* hides any content that overflows the table */  
+  overflow: hidden; /* hides any content that overflows the table */
 }
 
-table, th, td {
+table,
+th,
+td {
   border: 1px solid black;
 }
 
-th, td {
+th,
+td {
   padding: 4px 8px;
   text-align: center;
-  font-size: 0.8em; /* Smaller font size for larger matrices */ 
+  font-size: 0.8em; /* Smaller font size for larger matrices */
 }
 
 .blue-border {
-  border: 2px solid blue;
+  border: none;
+  /* border: 2px solid blue; */
 }
 
 .buttons-container {
@@ -168,32 +195,33 @@ th, td {
 .container {
   display: flex;
   justify-content: space-between;
-  align-items: center;
-  width: 100%; /* makes sure container takes full width available */ 
+  align-items: flex-start;
+  /* width: 100%; makes sure container takes full width available */
   padding: 2% 10%;
 }
 
 .divider {
   width: 2px; /* width of the vertical line */
-  height: 45vw; 
+  height: 45vw;
   background-color: gray; /* color of the vertical line */
   margin: 0 10px; /* . some spacing between the divider and the boxes */
 }
 
 .matrix-box {
-  width: 50vw; /* this ensures the box is a square */
-  height: 50vw; /* this ensures the box is a square */
-  font-size: 1.3em; 
-  overflow: auto; 
+  max-width: 45vw; /* this ensures the box is a square */
+  max-height: 85vw; /* this ensures the box is a square */
+  font-size: 1.0em;
+  overflow: auto;
+  margin-bottom: 20px;
 }
 
 .matrix-section {
   flex: 1;
-  /* width: 50vm;   */
+  padding: 5px;
 }
 
 .red-border {
-  border: none; 
+  border: none;
   /*border: 2px solid red;*/
 }
 
@@ -203,5 +231,4 @@ th, td {
   align-items: center;
   padding: 1% 5%;
 }
-
 </style>
--- rust-backend/src/main.rs
+++ rust-backend/src/main.rs
@@ -1,15 +1,21 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 extern crate rocket_cors;
 
 use rand::Rng;
 use rocket::http::Method;
-use rocket_contrib::json::{ Json, JsonValue };
+use rocket::{response, State};
+use rocket_contrib::json::{Json, JsonValue};
 use rocket_cors::{AllowedHeaders, AllowedOrigins, CorsOptions};
 use serde_derive::Serialize;
+use simple_logger::SimpleLogger;
+use std::cmp;
 use std::sync::Mutex;
-use rocket::State;
+
+use log::LevelFilter;
+use log::{error, info};
 
 type Matrix = Vec<Vec<f64>>;
 
@@ -22,6 +28,7 @@ struct MatrixData {
 #[derive(Serialize)]
 struct SolutionResponse {
     solution: Option<Vec<f64>>,
+    error: Option<String>,
 }
 
 #[derive(Serialize)]
@@ -29,64 +36,58 @@ struct MatrixResponse {
     matrix: Matrix,
 }
 
-
 fn main() {
+    SimpleLogger::new()
+        .with_level(LevelFilter::Info)
+        .init()
+        .unwrap();
+
     let allowed_origins = AllowedOrigins::some_exact(&["http://localhost:3000"]);
 
     let cors = CorsOptions {
         allowed_origins,
-        allowed_methods: vec![Method::Get, Method::Post ].into_iter().map(From::from).collect(),
-        allowed_headers: AllowedHeaders::some(&["Authorization", "Accept"]),
+        allowed_methods: vec![Method::Get, Method::Post]
+            .into_iter()
+            .map(From::from)
+            .collect(),
+        allowed_headers: AllowedHeaders::some(&[
+            "Authorization",
+            "Accept",
+            "Content-type",
+            "Cache-Control",
+        ]),
         allow_credentials: true,
         ..Default::default()
     }
     .to_cors()
     .unwrap();
 
-    let matrix_data = Mutex::new( MatrixData {
+    let matrix_data = Mutex::new(MatrixData {
         matrix: Vec::new(),
         b: Vec::new(),
         solution: None,
     });
 
     rocket::ignite()
-        .manage( matrix_data )
-        .mount("/", routes![ generate_matrix, solve_matrix, get_solution ])
+        .manage(matrix_data)
+        .mount("/", routes![generate_matrix, solve_matrix, get_solution])
         .attach(cors)
         .launch();
 }
 
-fn generate_sparse_matrix(m: usize, n: usize, s: f64) -> Matrix {
-    let mut rng = rand::thread_rng();
-    let mut matrix = vec![vec![0.0; n]; m];
-
-    let total_elements = m * n;
-    let non_zero_elements = (s * total_elements as f64).ceil() as usize;
-
-    for _ in 0..non_zero_elements {
-        let mut row = rng.gen_range(0..m);
-        let mut col = rng.gen_range(0..n);
-
-        while matrix[row][col] != 0.0 {
-            row = rng.gen_range(0..m);
-            col = rng.gen_range(0..n);
-        }
-
-        // Generate a random float and format it to 2 decimal places
-        let value = rng.gen::<f64>();
-        let formatted_value = format!("{:.2}", value).parse::<f64>().unwrap();
-        matrix[row][col] = formatted_value 
-    }
-    matrix
-}
-
-fn solve_sparse_matrix_jacobi(matrix: Vec<Vec<f64>>, b: Vec<f64>, tolerance: f64) -> Vec<f64> {
+fn solve_sparse_matrix_jacobi(
+    matrix: Vec<Vec<f64>>,
+    b: Vec<f64>,
+    tolerance: f64,
+) -> Result<Vec<f64>, String> {
     let n = matrix.len();
-    let mut x = vec![0.0; n]; // Initial guess
+    let mut x = vec![0.0; n];
     let mut new_x = x.clone();
-    let mut error;
+    let max_iterations = 10000; // Set a maximum for iterations
+    let mut iterations = 0;
 
-    loop {
+    while iterations < max_iterations {
+        iterations += 1;
         for i in 0..n {
             let mut sigma = 0.0;
             for j in 0..n {
@@ -96,13 +97,59 @@ fn solve_sparse_matrix_jacobi(matrix: Vec<Vec<f64>>, b: Vec<f64>, tolerance: f64
             }
             new_x[i] = (b[i] - sigma) / matrix[i][i];
         }
-        error = new_x.iter().zip(x.iter()).fold(0.0, |acc, (&new, &old)| acc + (new - old).abs());
+
+        // Check for convergence
+        let error: f64 = new_x
+            .iter()
+            .zip(x.iter())
+            .map(|(&new, &old)| (new - old).abs())
+            .sum();
         if error < tolerance {
-            break;
+            return Ok(new_x);
         }
         x = new_x.clone();
     }
-    x
+    Err("Maximum iterations reached without convergence".to_string())
+}
+
+
+fn generate_sparse_matrix(m: usize, n: usize, s: f64) -> Matrix {
+    let mut rng = rand::thread_rng();
+    let mut matrix = vec![vec![0.0; n]; m];
+
+    // Generate the sparse elements
+    let total_elements = m * n;
+    let non_zero_elements = (s * total_elements as f64).ceil() as usize;
+
+    for _ in 0..non_zero_elements {
+        let mut row = rng.gen_range(0..m);
+        let mut col = rng.gen_range(0..n);
+
+        // Ensure we don't overwrite the diagonal elements
+        while matrix[row][col] != 0.0 || row == col {
+            row = rng.gen_range(0..m);
+            col = rng.gen_range(0..n);
+        }
+
+        // Generate a random float and format it to 2 decimal places
+        let value = rng.gen::<f64>();
+        matrix[row][col] = format!("{:.2}", value).parse::<f64>().unwrap();
+    }
+
+    // Post-processing to ensure non-zero diagonals and diagonal dominance
+    for i in 0..cmp::min(m, n) {
+        // Generate and format the diagonal element
+        let diagonal_value = rng.gen_range(1.0..2.0);
+        matrix[i][i] = format!("{:.2}", diagonal_value).parse::<f64>().unwrap();
+
+        // Ensure diagonal dominance
+        let row_sum: f64 = matrix[i].iter().filter(|&&x| x != 0.0).sum();
+        matrix[i][i] += row_sum;
+        // Format the updated diagonal value to two decimal places
+        matrix[i][i] = format!("{:.2}", matrix[i][i]).parse::<f64>().unwrap();
+    }
+
+    matrix
 }
 
 // Generate a sparse matrix and vector b
@@ -113,25 +160,49 @@ fn generate_matrix(state: State<Mutex<MatrixData>>) -> Json<MatrixResponse> {
     data.b = vec![1.0; 7]; // Example values for b
     data.solution = None;
 
+    info!("Generated matrix: {:?}", data.matrix);
+
     Json(MatrixResponse {
-        matrix: data.matrix.clone()
+        matrix: data.matrix.clone(),
     })
 }
 
-// Solve the matrix
 #[post("/solveMatrix")]
-fn solve_matrix(state: State<Mutex<MatrixData>>) {
+fn solve_matrix(state: State<Mutex<MatrixData>>) -> Json<SolutionResponse> {
     let mut data = state.lock().unwrap();
+    let mut response_error = None;
+
     if data.solution.is_none() {
         let tolerance = 1e-6;
-        data.solution = Some(solve_sparse_matrix_jacobi(data.matrix.clone(), data.b.clone(), tolerance));
+        match solve_sparse_matrix_jacobi(data.matrix.clone(), data.b.clone(), tolerance) {
+            Ok(solution) => {
+                data.solution = Some(solution);
+                info!("Solved matrix, solution: {:?}", data.solution);
+            }
+            Err(message) => {
+                error!("Error solving matrix: {}", message);
+                // Set the error field in the response
+                response_error = Some(message);
+                // Ensure the solution field is None
+                data.solution = None;
+            }
+        }
+    } else {
+        info!("Matrix already solved");
     }
+
+    // Construct your JSON response
+    Json(SolutionResponse {
+        solution: data.solution.clone(),
+        error: response_error,
+    })
 }
 
 #[get("/getSolution")]
 fn get_solution(state: State<Mutex<MatrixData>>) -> Json<SolutionResponse> {
     let data = state.lock().unwrap();
     Json(SolutionResponse {
-        solution: data.solution.clone()
+        solution: data.solution.clone(),
+        error: None,
     })
 }
